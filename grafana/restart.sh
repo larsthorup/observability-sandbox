e04097fb0a1687d0ece35d82749aefe4a20901d2
Lars Thorup <lars@zealake.com>
2021-08-28 10:10:03 +0200

    grafana dashboard

--- grafana/restart.sh
+++ grafana/restart.sh
@@ -3,7 +3,7 @@ docker rm grafana
 docker run -d \
   --name grafana \
   --link prometheus \
-  -v "$(pwd)/data/grafana.ini":/etc/grafana/grafana.ini \
+  -v "$(pwd)/data":/etc/grafana \
   -p 3000:3000 \
   grafana/grafana
 docker logs -f grafana
